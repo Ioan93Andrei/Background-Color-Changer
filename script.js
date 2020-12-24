057aa5ec02e70eda7fa21b1420120c16c2ec395b
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2020-12-24 09:49:18 +0100

    now the color is shown correctly even by the paraghraf even though it's in rgb.

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ const button = document.querySelector("#main_button");
 
 function randomColor(){
   let letters = "0123456789ABCDEF";
-  let color = "#";
+  let hash = "#";
   for(let i = 0; i < 6; i++){
-    color += letters[Math.floor(Math.random() * 16)];
+    hash += letters[Math.floor(Math.random() * 16)];
   }
-  return color;
+  return hash;
 };
+
+let color = randomColor();
 
 function changeBackground(){
-  document.body.style.backgroundColor = randomColor();
+  let current_color = document.body.style.backgroundColor = randomColor();
+  return current_color
 };
 
-function createParagraph(){
-  let color = randomColor();
-  const div = document.querySelector(".container");
-  let par = document.createElement("p");
+function editParagraph(){
+  let par = document.querySelector(".color_par");
+  color = document.body.style.backgroundColor;
   par.innerHTML = "Current color is " + color;
-  div.appendChild(par);
 }
 
+button.addEventListener("click", randomColor);
 button.addEventListener("click", changeBackground);
-button.addEventListener("click", createParagraph);
+button.addEventListener("click", editParagraph);
+console.log(randomColor());
