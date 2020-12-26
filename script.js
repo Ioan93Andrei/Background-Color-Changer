e08e56733db7723c3f0a27b81926816cbfaa55b1
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2020-12-26 11:39:16 +0100

    the app finally shows the proper HEX code and the code has been refactored.

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-const button = document.querySelector("#main_button");
-
+// Is OK to be globally accessible
 function randomColor(){
   let letters = "0123456789ABCDEF";
-  let hash = "#";
+  let color = "#";
   for(let i = 0; i < 6; i++){
-    hash += letters[Math.floor(Math.random() * 16)];
+    color += letters.charAt(Math.floor(Math.random() * 16));
   }
-  return hash;
-};
-
-let color = randomColor();
-
-function changeBackground(){
-  let current_color = document.body.style.backgroundColor = randomColor();
-  return current_color
-};
-
-function editParagraph(){
-  let par = document.querySelector(".color_par");
-  color = document.body.style.backgroundColor;
-  par.innerHTML = "Current color is " + color;
+  return color;
 }
 
-button.addEventListener("click", randomColor);
-button.addEventListener("click", changeBackground);
-button.addEventListener("click", editParagraph);
-console.log(randomColor());
+// Should be placed inside an IIFE; the global context is still accessible
+(function() {
+  const button = document.querySelector('#main_button');
+  const pElement = document.querySelector('#p_id');
+
+  button.addEventListener('click', function() {
+    let color = randomColor();
+    document.body.style.background = color;
+    pElement.textContent = 'Current Color is ' + color;
+  });
+})();
